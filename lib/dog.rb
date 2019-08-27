de37df95961d777f4c3928f8a5308c8dcbe8bd71
github username <github email address>
2019-08-26 21:08:12 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+    def initialize(dog_name='')
+        @name = dog_name
+    end
+    
+    def name #getter method
+        @name
+    end
+    
+    def name=(dog_name) #setter method
+        @name = dog_name
+    end
+
+    def breed 
+        @breed
+    end
+
+    def breed= (dog_breed)
+        @breed= dog_breed
+    end
+    
+    
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+ 
+    def initialize(name='')
+        @name = name
+    end
+    
+    def name #getter method
+        @name
+    end
+    
+    def name=(name) #setter method
+        @name = name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+  end
